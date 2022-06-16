d0d3d317a734d3c4bf7e4cd2a389fe223920388a
Jacob <fabjacob@gmail.com>
2022-06-16 14:03:33 -0300

    Add files via upload

---
+++ Gruntfile.js
@@ -0,0 +1,99 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: ['lib/**/*.js', 'public/js/*.js'],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+        mangle: false
+      },
+      build: {
+        files: {
+          'public/js/dist/login.min.js': 'public/js/login.js',
+          'public/js/dist/app.min.js': 'public/js/app.js',
+          'public/js/dist/dodgercms.min.js': 'lib/**/*.js'
+        }
+      },
+    },
+    cssmin: {
+      combine: {
+        files: {
+          'public/css/dist/login.min.css': 'public/css/login.css',
+          'public/css/dist/app.min.css': 'public/css/app.css',
+          'public/css/vendor/jstree.proton.min.css': 'public/css/vendor/jstree.proton.css'
+        }
+      }
+    },
+    handlebars: {
+      compile: {
+        options: {
+          namespace: "dodgercms.templates",
+          processName: function(filePath) {
+            var pieces = filePath.split("/");
+            return pieces[pieces.length - 1];
+          }
+        },
+        files: {
+          "public/js/dist/entry.min.js": "templates/entry.html"
+        }
+      }
+    },
+    mocha: {
+      all: {
+        src: ['test/runner.html'],
+      },
+      options: {
+        run: true
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['lib/**/*.js', 'public/js/*.js'],
+        tasks: ['uglify'],
+        options: {
+          spawn: false,
+        }
+      }
+    },
+    s3: {
+      options: {
+        key: '<%= aws.key %>',
+        secret: '<%= aws.secret %>',
+        bucket: '<%= aws.bucket %>',
+        access: 'private'
+      },
+      dev: {
+        upload: [
+          {
+            src: 'index.html',
+            dest: 'index.html'
+          },
+          {
+            src: 'login.html',
+            dest: 'login.html'
+          },
+          {
+            src: 'public/**',
+            dest: 'public/**'
+          },
+        ]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-handlebars');
+  grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-s3');
+
+  grunt.registerTask('default', ['handlebars', 'mocha', 'cssmin', 'uglify']);
+  grunt.registerTask('deploy', ['default', 's3']);
+};
---
+++ README.md
@@ -0,0 +1,35 @@
+[![DodgerCMS](http://i.imgur.com/EmVj8OL.png)](http://dodgercms.com/)
+
+[![Build Status](https://travis-ci.org/ChrisZieba/dodgercms.svg)](https://travis-ci.org/ChrisZieba/dodgercms)
+ 
+[DodgerCMS](http://dodgercms.com) is a static markdown CMS built on top of [Amazon S3](http://aws.amazon.com/s3/). It is a clean and *simple* alternative to heavy content management systems. There are no databases to manage, deployments to monitor, or massive configuration files. Just focus on writing your content and the results are live immediately.
+
+## Demo
+
+The [demo](http://dodgercms.com) is used to display the documentation for DodgerCMS. It displays the default layout and style of the front end. To see screenshots of the backend manager, please take a look [here](http://dodgercms.com/#screenshots).
+
+## Features
+
+- [Full markdown editor](http://dodgercms.com/features/editor)
+- [Uploads images directly to the cloud](http://dodgercms.com/features/images)
+- [Handles changes in URL structure](http://dodgercms.com/features/menu)
+- [Access via your S3 bucket](http://dodgercms.com/features/cloud)
+- [Custom layouts](http://dodgercms.com/features/layouts)
+- [Live updates](http://dodgercms.com/features/live)
+
+## Documentation
+
+The [documentation](http://dodgercms.com) for DodgerCMS is itself built with DodgerCMS.
+
+## Installation
+
+Please take a look at the [installation guide](http://dodgercms.com/help/installation).
+
+## Development
+
+DodgerCMS is still very early in its development. Please take a look at the development guide for more information on how to [contribute](http://dodgercms.com/development/contributing).
+
+## License
+
+`MIT License` (MIT)  
+Copyright (c) 2015 Chris Zieba
